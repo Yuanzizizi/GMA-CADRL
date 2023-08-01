c431a01edbf93b4bcc09e1152858e8d65d58b6cc
Y <105049539+Yuanzizizi@users.noreply.github.com>
2023-08-01 10:09:31 +0800

    Update UnicycleDynamics.py

--- gym_collision_avoidance/gym_collision_avoidance/envs/dynamics/UnicycleDynamics.py
+++ gym_collision_avoidance/gym_collision_avoidance/envs/dynamics/UnicycleDynamics.py
@@ -1,5 +1,3 @@
-
-
 import numpy as np
 from gym_collision_avoidance.envs import Config
 from gym_collision_avoidance.envs.dynamics.Dynamics import Dynamics
@@ -8,7 +6,6 @@ import math
 
 class UnicycleDynamics(Dynamics):
     """ Convert a speed & heading to a new state according to Unicycle Kinematics model.
-
     """
 
     def __init__(self, agent):
@@ -16,17 +13,14 @@ class UnicycleDynamics(Dynamics):
 
     def step(self, action, dt):
         """ 
-
         In the global frame, assume the agent instantaneously turns by :code:`heading`
         and moves forward at :code:`speed` for :code:`dt` seconds.  
-        Add that offset to the current position. Update the velocity in the
-        same way. Also update the agent's turning direction (only used by CADRL).
-
+        
         Args:
             action (list): [delta heading angle, speed] command for this agent
             dt (float): time in seconds to execute :code:`action`
-    
         """
+        
         selected_speed = action[0]
         selected_heading = wrap(action[1] + self.agent.heading_global_frame)
 
@@ -43,11 +37,3 @@ class UnicycleDynamics(Dynamics):
         self.agent.delta_heading_global_frame = wrap(selected_heading -
                                                self.agent.heading_global_frame)
         self.agent.heading_global_frame = selected_heading
-
-        # turning dir: needed for cadrl value fn
-        if abs(self.agent.turning_dir) < 1e-5:
-            self.agent.turning_dir = 0.11 * np.sign(selected_heading)
-        elif self.agent.turning_dir * selected_heading < 0:
-            self.agent.turning_dir = max(-np.pi, min(np.pi, -self.agent.turning_dir + selected_heading))
-        else:
-            self.agent.turning_dir = np.sign(self.agent.turning_dir) * max(0.0, abs(self.agent.turning_dir)-0.1)
