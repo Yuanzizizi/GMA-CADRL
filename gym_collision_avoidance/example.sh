ceecea3f5cb34d98cee0d8808d023193878773ec
Y <105049539+Yuanzizizi@users.noreply.github.com>
2023-08-01 09:50:12 +0800

    Update example.sh

--- gym_collision_avoidance/example.sh
+++ gym_collision_avoidance/example.sh
@@ -10,4 +10,4 @@ print_header "Running example python script"
 cd $DIR
 
 # python src/generate_pickle.py
-python src/example.py --checkpt_name "v16b_full_noupdatefmm_same_sd_ratio1_03095000" --case_id 0 --agent_num 5 --mini " "
+python src/example.py --checkpt_name "gma-cadrl" --case_id 0 --agent_num 5 --mini " "
